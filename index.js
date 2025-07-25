a74a385eecc7858a5a90da1de3e47e04a383314a
nrtdev <dev@newrise.in>
2025-07-25 10:33:07 +0530

    changes in index file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path")
+
 const authRoutes = require("./routes/authRoutes");
 const blogPostsRoute = require("./routes/blogPostsRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -11,16 +14,21 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 const subsribeRoutes = require("./routes/subscribeRoutes")
 
-const cors = require("cors");
 
 //Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 //Connect to the database
 connectDB()
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.error("Database connection error", err));
+
+  // Routes
+  app.get("/", (req, res) => {
+  res.send("Welcome to the Blog API!");
+});
 
 //Routes
 app.use("/api/auth", authRoutes);
@@ -32,13 +40,14 @@ app.use("/api/dislikes", disLikeRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/subscribe",subsribeRoutes)
-app.use("/uploads", express.static("uploads"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Blog API!");
-});
+const uploadsPath = path.join(__dirname,"uploads")
+app.use("/uploads", express.static(uploadsPath));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app
